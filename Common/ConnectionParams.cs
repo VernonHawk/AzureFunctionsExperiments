49fb9f5a0d297f61669888748be045d42f0a77a1
Igor Morenets <igor.morenec@gmail.com>
2020-05-03 22:36:32 +0300

    Revert common

--- Common/ConnectionParams.cs
+++
@@ -1,9 +0,0 @@
-namespace Common
-{
-    public static class ConnectionParams
-    {
-        public const string DbConnectionStringSetting = "PostsDBConnectionString";
-        public const string DatabaseName = "Study";
-        public const string CollectionName = "Posts";
-    }
-}
