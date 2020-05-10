38766cf549bbdf35c2bd162d71f0ab1a908e1375
Igor Morenets <igor.morenec@gmail.com>
2020-05-10 19:37:29 +0300

    Prettify code

--- ServiceGlue/Emailer.cs
+++ ServiceGlue/Emailer.cs
@@ -11,57 +11,57 @@ namespace ServiceGlue
     public static class Emailer
     {
 [FunctionName("Emailer")]
-public static async Task RunAsync(
-    [CosmosDBTrigger(
-        databaseName: ConnectionParams.Database,
-        collectionName: ConnectionParams.PostsCollection,
-        ConnectionStringSetting = ConnectionParams.DbConnectionStringSetting,
-        CreateLeaseCollectionIfNotExists = true,
-        LeaseCollectionName = "leases"
-    )]
-    IReadOnlyList<Document> posts,
-    [CosmosDB(
-        databaseName: ConnectionParams.Database,
-        collectionName: ConnectionParams.PostsCollection,
-        ConnectionStringSetting = ConnectionParams.DbConnectionStringSetting
-    )]
-    DocumentClient client,
-    [SendGrid(ApiKey = "SendGridApiKey")] IAsyncCollector<SendGridMessage> collector
-)
-{
-    var authorsUri = UriFactory.CreateDocumentCollectionUri(
-        databaseId: ConnectionParams.Database,
-        collectionId: ConnectionParams.AuthorsCollection
-    );
-
-    foreach (var doc in posts)
-    {
-        var author = doc.GetPropertyValue<string>("Author");
-
-        var messages = client
-                       .CreateDocumentQuery<Author>(
-                           documentCollectionUri: authorsUri,
-                           feedOptions: new FeedOptions {EnableCrossPartitionQuery = true}
-                       )
-                       .Where(a => a.Name == author)
-                       .Select(a => a.Subscribers)
-                       .ToList()
-                       .FirstOrDefault()
-                       .Select(
-                           sub => ComposeMessage(
-                               from: "i.morenets@ukma.edu.ua",
-                               to: sub,
-                               author: author,
-                               title: doc.GetPropertyValue<string>("Title")
-                           )
-                       );
-
-        foreach (var message in messages)
+        public static async Task EmailerFun(
+            [CosmosDBTrigger(
+                databaseName: ConnectionParams.Database,
+                collectionName: ConnectionParams.PostsCollection,
+                ConnectionStringSetting = ConnectionParams.DbConnectionStringSetting,
+                CreateLeaseCollectionIfNotExists = true,
+                LeaseCollectionName = "leases"
+            )]
+            IReadOnlyList<Document> posts,
+            [CosmosDB(
+                databaseName: ConnectionParams.Database,
+                collectionName: ConnectionParams.PostsCollection,
+                ConnectionStringSetting = ConnectionParams.DbConnectionStringSetting
+            )]
+            DocumentClient client,
+            [SendGrid(ApiKey = "SendGridApiKey")] IAsyncCollector<SendGridMessage> collector
+        )
         {
-            await collector.AddAsync(message);
+            var authorsUri = UriFactory.CreateDocumentCollectionUri(
+                databaseId: ConnectionParams.Database,
+                collectionId: ConnectionParams.AuthorsCollection
+            );
+
+            foreach (var doc in posts)
+            {
+                var author = doc.GetPropertyValue<string>("Author");
+
+                var messages = client
+                               .CreateDocumentQuery<Author>(
+                                   documentCollectionUri: authorsUri,
+                                   feedOptions: new FeedOptions {EnableCrossPartitionQuery = true}
+                               )
+                               .Where(a => a.Name == author)
+                               .Select(a => a.Subscribers)
+                               .ToList()
+                               .FirstOrDefault()
+                               .Select(
+                                   sub => ComposeMessage(
+                                       from: "i.morenets@ukma.edu.ua",
+                                       to: sub,
+                                       author: author,
+                                       title: doc.GetPropertyValue<string>("Title")
+                                   )
+                               );
+
+                foreach (var message in messages)
+                {
+                    await collector.AddAsync(message);
+                }
+            }
         }
-    }
-}
 
         private static SendGridMessage ComposeMessage(
             string from,
